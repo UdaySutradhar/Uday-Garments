e99d44eba8d048a5e4017b3904baf1ee5971f92e
UdaySutradhar <sutradharuday23@kgpian.iitkgp.ac.in>
2024-01-26 14:09:52 +0530

    updation

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -13,6 +13,17 @@ const CartItems = () => {
                 <p>Total</p>
                 <p>Remove</p>
             </div>
+            <hr />
+            <div>
+                <div className="cartitems-format">
+                    <img src="" alt="" className='carticon-product-icon'/>
+                    <p></p>
+                    <p></p>
+                    <button className='cartitems-quantity'></button>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
